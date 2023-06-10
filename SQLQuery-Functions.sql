5deeb37902d692fcf70b162b7533061e50804fc2
danyrenghea <79364899+danyrenghea@users.noreply.github.com>
2023-06-10 15:21:22 +0300

    Create SQLQuery-Functions.sql
    fifth commit

---
+++ SQLQuery-Functions.sql
@@ -0,0 +1,22 @@
+SELECT CONCAT ('google','.com') AS ConcatFunction
+
+SELECT ASCII (ProductID) AS NumberCodeOfID
+FROM Products
+
+SELECT ASCII('0')
+
+SELECT CHAR (64) AS CodeToCharacter
+
+SELECT LOWER ('SQL Functions') AS FunctiaLOWER
+
+SELECT UPPER ('SQL Functions')AS FunctiaUPPER 
+
+SELECT REPLACE ( 'TUTORIAL', 'T', 't')AS InlocuireaUnorElemente 
+
+SELECT GETDATE ()AS DataSiOraCurenta
+
+SELECT DAY('2023-06-04') AS ZiuaDinData
+
+SELECT MONTH ('2023-06-04') AS LunaDinData
+
+SELECT YEAR (2023-06-04) AS Anul
